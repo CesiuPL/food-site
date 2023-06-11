32bcc8a1de7a7cfb90c5d30990e06614e34df81a
Przemek Cesarz <przemek.cesiu@gmail.com>
2023-06-12 00:42:03 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+import { CartPageComponent } from './cart-page/cart-page/cart-page.component';
+import { FoodPageComponent } from './food-page/food-page/food-page.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
+import { TagsComponent } from './tags/tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    CartPageComponent,
+    FoodPageComponent,
+    NotFoundComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RatingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tags/tags/tags.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food/food.service';
+import { Tag } from 'src/app/shared/models/Tag';
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.scss']
+})
+export class TagsComponent implements OnInit {
+
+  @Input()
+  foodPageTags?:string[];
+
+  @Input()
+  justifyContent:string = 'center';
+
+  tags?:Tag[];
+  constructor(private foodService:FoodService) { }
+
+  ngOnInit(): void {
+    if(!this.foodPageTags)
+     this.tags = this.foodService.getAllTags();
+  }
+
+}
